14dbc7741385a7f943988452a7af69e59ddfa587
Pedro - GitPro <peidom81@gmail.com>
2020-10-20 17:47:58 -0400

    Video 8

---
+++ notes_ddd_course/lib/presentation/core/app_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:notes_ddd_course/presentation/signin/sign_in_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes on DDD',
+      theme: ThemeData.light().copyWith(
+        primaryColor: Colors.green[800],
+        accentColor: Colors.blueAccent,
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: SignInPage(),
+    );
+  }
+}
---
+++ notes_ddd_course/lib/presentation/signin/sign_in_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_ddd_course/application/auth/sign_in_form/sign_in_form_bloc.dart';
+import 'package:notes_ddd_course/injection.dart';
+import 'package:notes_ddd_course/presentation/signin/widgets/sign_in_form.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'),
+      ),
+      body: BlocProvider<SignInFormBloc>(
+        create: (context) => getIt<SignInFormBloc>(),
+        child: const SignInForm(),
+      ),
+    );
+  }
+}
