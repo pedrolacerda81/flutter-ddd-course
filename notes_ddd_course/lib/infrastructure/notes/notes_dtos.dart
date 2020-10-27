9b2acb757935101a41ae01b11f50cdd7acbf3aec
Pedro - GitPro <peidom81@gmail.com>
2020-10-27 15:58:03 -0400

    Video 14 pt 2

--- notes_ddd_course/lib/infrastructure/notes/notes_dtos.dart
+++ notes_ddd_course/lib/infrastructure/notes/notes_dtos.dart
@@ -1,10 +1,64 @@
+import 'dart:ui';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:notes_ddd_course/domain/core/value_objects.dart';
+import 'package:notes_ddd_course/domain/notes/note.dart';
 import 'package:notes_ddd_course/domain/notes/todo_item.dart';
 import 'package:notes_ddd_course/domain/notes/value_objects.dart';
+import 'package:kt_dart/kt.dart';
 
 part 'notes_dtos.freezed.dart';
 part 'notes_dtos.g.dart';
+
+@freezed
+abstract class NoteDto implements _$NoteDto {
+  const NoteDto._();
+
+  const factory NoteDto({
+    @JsonKey(ignore: true) String id,
+    @required String body,
+    @required int color,
+    @required List<TodoItemDto> todos,
+    @required @ServerTimeStampConverter() FieldValue serverTimeStamp,
+  }) = _NoteDto;
+
+  Note toDomain() => Note(
+        id: UniqueId.fromUniqueString(id),
+        body: NoteBody(body),
+        color: NoteColor(Color(color)),
+        toDos: List3(todos.map((dto) => dto.toDomain()).toImmutableList()),
+      );
+
+  factory NoteDto.fromDomain(Note note) {
+    return NoteDto(
+      id: note.id.getOrCrash(),
+      body: note.body.getOrCrash(),
+      color: note.color.getOrCrash().value,
+      todos: note.toDos
+          .getOrCrash()
+          .map((todoItem) => TodoItemDto.fromDomain(todoItem))
+          .asList(),
+      serverTimeStamp: FieldValue.serverTimestamp(),
+    );
+  }
+
+  factory NoteDto.fromJson(Map<String, dynamic> json) =>
+      _$NoteDtoFromJson(json);
+
+  factory NoteDto.fromFirestore(DocumentSnapshot doc) {
+    return NoteDto.fromJson(doc.data).copyWith(id: doc.documentID);
+  }
+}
+
+class ServerTimeStampConverter implements JsonConverter<FieldValue, Object> {
+  const ServerTimeStampConverter();
+
+  @override
+  FieldValue fromJson(Object json) => FieldValue.serverTimestamp();
+
+  @override
+  Object toJson(FieldValue fieldValue) => fieldValue;
+}
 
 @freezed
 abstract class TodoItemDto implements _$TodoItemDto {
